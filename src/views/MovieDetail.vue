76f26f71b2f4024e4727312ec4bf9fe5adf3d1d7
tiger-githubb <aristidetiger12@gmail.com>
2022-07-21 07:46:38 +0000

    debut de la fonction page details

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,15 +1,38 @@
 <template>
+
   <div class="movie-detail">
-    detail {{$route.params.id}}
+    <h2>movie title</h2>
   </div>
+
 </template>
 
 <script>
-export default {
+//onBeforeMount est une methode vue
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from '@/env';
 
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+    });
+
+    return {
+    movie
+    }
+  }
 }
+
+ 
 </script>
 
 <style>
-
 </style>
