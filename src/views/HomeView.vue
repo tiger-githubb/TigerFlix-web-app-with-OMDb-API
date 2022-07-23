ada0994b0b23be1bdfd71c057f940fb7925ce5a0
tiger-githubb <aristidetiger12@gmail.com>
2022-07-23 22:15:47 +0000

    style and media query

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,9 @@
         <img src="https://fr.web.img4.acsta.net/r_654_368/newsv7/21/01/11/16/11/2598562.jpg" alt="Spiderman"
           class="featured-img">
         <div class="detail">
-          <h3>Spider-man</h3>
-          <p>Entre rumeurs et informations fiables, le casting de "Spider-Man 3" a agité la Toile ces dernières
+          <h3 translate="no">Spider-man</h3>
+          <p translate="no">Entre rumeurs et informations fiables, le casting de "Spider-Man 3" a agité la Toile ces
+            dernières
             semaines. Pour vous
             permettre d'y voir plus clair et démêler le vrai du faux, voici le nom des personnages qui ont déjà été
             confirmés.</p>
@@ -18,7 +19,7 @@
 
     <form @submit.prevent="SearchMovies()" class="search-box">
       <input type="text" placeholder="quel film voulez-vous voir ..." v-model="search" />
-      <input type="submit" value="recherche" translate="no" >
+      <input type="submit" value="recherche" translate="no">
     </form>
   </div>
 
@@ -87,7 +88,6 @@ export default {
       width: 100%;
       height: 300px;
       object-fit: cover;
-
       position: relative;
       z-index: 0;
     }
@@ -97,8 +97,8 @@ export default {
       left: 0;
       right: 0;
       bottom: 0;
-      background-color: rgba(0, 0, 0, 0.6);
       padding: 16px;
+      padding-top: 100%;
       z-index: 1;
 
       h3 {
@@ -129,7 +129,9 @@ export default {
       &[type="text"] {
         width: 100%;
         color: #ffff;
-        background-color: #496583;
+        background-color: #3b41493b;
+        border: 2px solid #B90020;
+        border-radius: 15px;
         font-size: 20px;
         padding: 10px 16px;
         border-radius: 8px;
@@ -139,6 +141,7 @@ export default {
 
       &::placeholder {
         color: #f3f3f3;
+        text-transform: capitalize;
       }
 
       &:focus {
@@ -148,7 +151,7 @@ export default {
       &[type="submit"] {
         width: 100%;
         max-width: 300px;
-        background-color: #428883;
+        background-color: #B90020;
         padding: 16px;
         border-radius: 8px;
         color: #ffff;
@@ -167,8 +170,8 @@ export default {
 }
 
 img {
-  max-width : 100%;
-height : auto;
+  max-width: 100%;
+  height: auto;
 }
 
 .type {
@@ -204,11 +207,19 @@ height : auto;
   margin: 0px 8px;
 }
 
+@media (min-width: 760px) {
+  .detail {
+    background: linear-gradient(1deg, #000102 3.07%, rgba(0, 17, 33, 0) 25.29%) !important;
+  }
+}
+
+
+
 .detail {
-  background-color: #496583;
+
   padding: 16px 8px;
   flex: 1 1 100%;
-  border-radius: 8px;
+  background: linear-gradient(4deg, #000102 12.07%, rgba(0, 3, 5, 0) 53.29%);
 }
 
 
